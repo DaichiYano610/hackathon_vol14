33c64291c262e5cc67068c0f67d1521c2f973db1
Daichi Yano <y_di_ch@icloud.com>
2024-02-10 15:32:20 +0900

    webサーバファイルの修正

--- app.py
+++ app.py
@@ -6,10 +6,12 @@ app = Flask(__name__)
 CORS(app)  # CORS有効化
 
 @app.route("/")
-def home():
-    return render_template("home.html")
+def index():
+    return render_template("index.html")
 
-
+@app.route("/asairi",methods = ['GET'])
+def page():
+    return render_template("asairi.html")
 
 
 #おまじない
