72f103ef3bdeb3c539f6d0711c93c8794ec4d452
Rimi Hirata <rimionbanana0524@outlook.jp>
2024-02-10 14:35:53 +0900

    [add]豆知識をランダムに返す

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask
+import random
+
+
+def knowledge(path):
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+            return random.choice(lines).strip()
+        
+    except FileNotFoundError:
+        print(f'FileNotFoundError')
+    
+for i in range(10):
+    print(knowledge("C:/Users/rimio/hack/coffe_timer/knowledges.txt"))
