afe149be6a9009e7cbadc29909c6408ec7ee0cf9
Daichi Yano <y_di_ch@icloud.com>
2024-02-11 14:23:58 +0900

    flask_corsの除去

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask,render_template,request
-from flask_cors import CORS
+#from flask_cors import CORS
 import knowledge
 
 #Flaskオブジェクトの生成
 app = Flask(__name__)
-CORS(app)  # CORS有効化
+#CORS(app)  # CORS有効化
 
 @app.route("/")
 def index():
